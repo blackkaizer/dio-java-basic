f5a17bfab3a15ff8f4b20d3fdcd9f722012a22e7
blackkaizer <elitonkfogaca@gmail.com>
2023-08-12 09:55:41 -0300

    Commit dos operadores Aritméticos e Atribuiçao

---
+++ tipos-variaveis/src/OperadoresJava.java
@@ -0,0 +1,31 @@
+public class OperadoresJava {
+    
+    public static void main(String[] args) {
+    // Operador: 
+    /* = Operadore de Atribuição */
+        String name = "Eliton";
+        name = "Daniel"; // Reeatribuir valor.
+        // Mais alguns exemplos:
+        // int idade = 22;
+        // double peso = 68.5;
+        // char sexo = 'M';
+        // boolean doadorOrgao = false;
+        // Date dataNascimento = new Date();
+
+        System.out.println(name);
+
+        /* Operadores Aritméticos */
+
+        // double soma = 10.5 + 15.7; // Soma
+        // int subtracao = 113 - 25; // Subtação
+        // int multiplicacao = 20 * 7; // Multiplicação
+        int divisao = 15 / 3; // Divisão
+        // int modulo = 18 % 3; // Modulo = Verificar resto da divisão
+        // double resultado = (10 * 7) + (20 / 4); // Multiplicação + Divisão
+
+        // ATENÇÃO! O operador de adição (+), quando utilizado em variáveis do tipo texto, realizará a “concatenação de textos”.
+
+        System.out.println(divisao);
+    }
+
+}
