42a24742ac9ce5f7485d3f3a98e9fa2e8a6fd2c0
Eliton <elitonkfogaca@gmail.com>
2023-08-13 15:50:38 -0300

    Aprendendo estrutura repetição com break, continue

---
+++ estruturas-repetição/estruturas-de-repeticao/src/EstruturaBreakContinue.java
@@ -0,0 +1,25 @@
+public class EstruturaBreakContinue {
+    // public static void main(String[] args) {
+	
+    //     for(int numero = 1; numero <=5; numero ++){
+    //         if(numero==3)
+    //             break;
+            
+    //         System.out.println(numero);
+            
+    //     }
+    //     //Para a impressão no numero 2.
+    // }
+    public static void main(String[] args) {
+	
+        for(int numero = 1; numero <=5; numero ++){
+            if(numero==3)
+                continue;
+            
+            System.out.println(numero);
+            
+        }
+        // Continua até ser impresso o numero 5
+    
+    }
+}
