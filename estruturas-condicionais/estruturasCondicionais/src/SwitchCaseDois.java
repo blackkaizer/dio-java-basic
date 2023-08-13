388c07e34b2283143e3a2904c05e8abe328e16f7
Eliton <elitonkfogaca@gmail.com>
2023-08-12 23:11:06 -0300

    Switch Case

---
+++ estruturas-condicionais/estruturasCondicionais/src/SwitchCaseDois.java
@@ -0,0 +1,39 @@
+public class SwitchCaseDois {
+    // Modo condicional convencional
+    // public static void main(String[] args) {
+	// 	String plano = "M"; //M / T
+		
+	// 	if(plano == "B") {
+	// 		System.out.println("100 minutos de ligação");
+	// 	}else if(plano == "M") {
+	// 		System.out.println("100 minutos de ligação");
+	// 		System.out.println("WhatsApp e Instagram grátis");	
+	// 	}else if(plano == "T") {
+	// 		System.out.println("100 minutos de ligação");
+	// 		System.out.println("WhatsApp e Instagram grátis");	
+	// 		System.out.println("5Gb Youtube");	
+	// 	}
+	// }
+
+    // Modo condicional switch/case
+    public static void main(String[] args) {
+		String plano = "M"; // M / T
+
+		switch (plano) {
+			case "T": {
+				System.out.println("5Gb Youtube");
+			}
+			case "M": {
+				System.out.println("WhatsApp e Instagram grátis");
+			}
+			case "B": {
+				System.out.println("100 minutos de ligação");
+			}
+		}
+	}
+
+    /*
+     * Se optarem por usar switch / case, estudem um pouco mais, 
+     * sobre os conceitos de continue, break e default.
+    */
+}
